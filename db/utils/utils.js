de4efaa530162908a0cf7e3a2b3cd8ddd0fc91be
sattysingh96 <satnam.bhatti.singh96@gmail.com>
2019-11-25 15:46:21 +0000

    created formatDate utils function for seeding, TDD included

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -1,5 +1,12 @@
-exports.formatDates = list => {};
+exports.formatDates = list => {
+  if (list.length === 0) return [];
+  else {
+    return list.map((timeObj) => {
+      return timeObj.created_at = new Date(timeObj.created_at);
+    });
+  }
+};
 
-exports.makeRefObj = list => {};
+exports.makeRefObj = list => { };
 
-exports.formatComments = (comments, articleRef) => {};
+exports.formatComments = (comments, articleRef) => { };
--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -5,8 +5,32 @@ const {
   formatComments,
 } = require('../db/utils/utils');
 
-describe('formatDates', () => {});
+describe('formatDates', () => {
+  it('When passed an empty array, return a new empty array', () => {
+    const input = [];
+    expect(formatDates(input)).to.deep.equal([]);
+    expect(formatDates(input)).to.not.equal(input);
+  });
+  it('When passed an array, do not mutate the array', () => {
+    const originalInput = [{}];
+    formatDates(originalInput);
+    expect(originalInput).to.equal(originalInput)
+  });
+  it('When passed an array containing an sql timestamp object, return a new array containing the equivalent JS date object', () => {
+    const output = formatDates([{ created_at: 1511354163389 }]);
+    expect(output[0]).to.be.an.instanceOf(Date);
+  });
+  it('When passed an array containing multiple sql timestamp objects, return a new array containing the equivalent JS date objects', () => {
+    const output = formatDates([{ created_at: 1511354163389 }, { created_at: 1479818163389 }, { created_at: 1385210163389 }])
+    expect(output[0]).to.be.an.instanceOf(Date);
+    expect(output[1]).to.be.an.instanceOf(Date);
+    expect(output[2]).to.be.an.instanceOf(Date);
+  });
 
-describe('makeRefObj', () => {});
 
-describe('formatComments', () => {});
+
+});
+
+describe('makeRefObj', () => { });
+
+describe('formatComments', () => { });
