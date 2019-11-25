5012ca6557a0ae5c2b6fc513d48d28684dde2ceb
sattysingh96 <satnam.bhatti.singh96@gmail.com>
2019-11-25 11:52:01 +0000

    added knexfile to gitignore & added pg linux pass details to knexfile

--- knexfile.js
+++ knexfile.js
@@ -13,16 +13,16 @@ const baseConfig = {
 const customConfig = {
   development: {
     connection: {
-      database: 'nc_news'
-      // user,
-      // password
+      database: 'nc_news',
+      user: 'satty',
+      password: 'password'
     }
   },
   test: {
     connection: {
-      database: 'nc_news_test'
-      // user,
-      // password
+      database: 'nc_news_test',
+      user: 'satty',
+      password: 'password'
     }
   }
 };
