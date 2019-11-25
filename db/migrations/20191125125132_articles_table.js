57272f4a4cac65d3a339aba4499c8a604a918028
sattysingh96 <satnam.bhatti.singh96@gmail.com>
2019-11-25 14:17:05 +0000

    Created schema for articles table

--- db/migrations/20191125125132_articles_table.js
+++ db/migrations/20191125125132_articles_table.js
@@ -6,17 +6,14 @@ exports.up = function (knex) {
       articlesTable.increments('article_id').primary();
       articlesTable.string('title').notNullable();
       articlesTable.text('body').notNullable();
-      articlesTable.integer('votes').notNullable();
-      articlesTable.
-
-
-
-
-
+      articlesTable.integer('votes').defaultTo(0)
+      articlesTable.string('topic').references('topics.slug');
+      articlesTable.string('author').references('users.username');
+      articlesTable.timestamp('created_at').defaultTo(knex.fn.now());
     });
-
 };
 
 exports.down = function (knex) {
-
+  console.log('removing articles table');
+  return knex.schema.dropTable('articles');
 };
