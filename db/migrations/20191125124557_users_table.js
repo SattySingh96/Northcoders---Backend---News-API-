0a23b2c85a8ea1e251ef99d8a50ba9d570d64cba
sattysingh96 <satnam.bhatti.singh96@gmail.com>
2019-11-25 14:27:59 +0000

    created schema for comments table

--- db/migrations/20191125124557_users_table.js
+++ db/migrations/20191125124557_users_table.js
@@ -1,11 +1,12 @@
 
 exports.up = function (knex) {
   console.log('Creating users table');
-  return knex.schema.createTable('users', (usersTable) => {
-    usersTable.string('username').primary();
-    usersTable.string('avatar_url').notNullable();
-    usersTable.string('name').notNullable();
-  });
+  return knex.schema.createTable('users',
+    (usersTable) => {
+      usersTable.string('username').primary();
+      usersTable.string('avatar_url').notNullable();
+      usersTable.string('name').notNullable();
+    });
 };
 
 exports.down = function (knex) {
