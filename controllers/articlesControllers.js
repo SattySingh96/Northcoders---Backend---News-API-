79c1696f006d08d70ed80fcdac9becb43cdaf7d4
sattysingh96 <satnam.bhatti.singh96@gmail.com>
2019-11-27 09:38:52 +0000

    Created GET method for /api/articles/:article_id endpoint, with error handling for 405, full TDD started

--- controllers/articlesControllers.js
+++ controllers/articlesControllers.js
@@ -1,13 +1,20 @@
 const { fetchArticlesById } = require('../models/articlesModel')
 
 
-exports.getArticlesById = (req, res, next) => {
-  console.log('sending articles')
-  fetchArticlesById(req.params.article_id)
-    .then((articles) => {
-      res.status(200).send({ articles })
-    })
-    .catch(next);
-}
+// exports.getArticlesById = (req, res, next) => {
+//   console.log('sending articles')
+//   fetchArticlesById(req.params.article_id)
+//     .then((articles) => {
+//       res.status(200).send({ articles })
+//     })
+//     .catch(next);
+// }
 
-exports.getArticlesById
+// exports.getArticlesById = (req, res, next) => {
+//   console.log('sending articles')
+//   fetchArticlesById(req.params)
+//     .then((articles) => {
+//       res.sendStatus(200)
+//     })
+//     .catch(next);
+// }
--- routers/articlesRouter.js
+++ routers/articlesRouter.js
@@ -1,9 +1,8 @@
 const articlesRouter = require('express').Router();
-const { getArticlesByID } = require('../controllers/articlesControllers');
+const { getArticlesById } = require('../controllers/articlesControllers')
 const { handle405s } = require('../errors/index')
 
-
-articlesRouter.route('/:article_id').get(getArticlesByID).all(handle405s)
+articlesRouter.route('/:article_id').get(getArticlesById).all(handle405s)
 
 
 
