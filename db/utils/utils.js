4c70da12616b854f1e764b12c71df4f491d1da37
Satnam Singh <Satnam.Bhatti.Singh96@gmail.com>
2019-11-30 10:59:13 +0000

    added HEROKU HOSTING functionality

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -1,35 +1,35 @@
 exports.formatDates = list => {
-  if (list.length === 0) return [];
-  else {
-    return list.map((obj) => {
-      obj.created_at = new Date(obj.created_at);
-      return obj;
-    });
-  }
+    if (list.length === 0) return [];
+    else {
+        return list.map((obj) => {
+            obj.created_at = new Date(obj.created_at);
+            return obj;
+        });
+    }
 };
 
 exports.makeRefObj = (list, key, value) => {
-  if (list.length === 0) return {};
-  else {
-    return list.reduce(function (lookUpObj, arrayObj) {
-      lookUpObj[arrayObj[key]] = arrayObj[value];
-      return lookUpObj;
-    }, {});
-  };
+    if (list.length === 0) return {};
+    else {
+        return list.reduce(function(lookUpObj, arrayObj) {
+            lookUpObj[arrayObj[key]] = arrayObj[value];
+            return lookUpObj;
+        }, {});
+    };
 };
 
 
 exports.formatComments = (comments, articleRef) => {
-  if (comments.length === 0) return comments;
-  else {
-    return comments.map((commentObj) => {
-      const newCommentObj = { ...commentObj };
-      newCommentObj.article_id = articleRef[newCommentObj.belongs_to]
-      delete newCommentObj.belongs_to;
-      newCommentObj.author = newCommentObj.created_by
-      delete newCommentObj.created_by
-      newCommentObj.created_at = new Date(newCommentObj.created_at);
-      return newCommentObj;
-    });
-  }
+    if (comments.length === 0) return comments;
+    else {
+        return comments.map((commentObj) => {
+            const newCommentObj = {...commentObj };
+            newCommentObj.article_id = articleRef[newCommentObj.belongs_to]
+            delete newCommentObj.belongs_to;
+            newCommentObj.author = newCommentObj.created_by
+            delete newCommentObj.created_by
+            newCommentObj.created_at = new Date(newCommentObj.created_at);
+            return newCommentObj;
+        });
+    }
 };
--- knexfile.js
+++ knexfile.js
@@ -1,30 +1,34 @@
+const { DB_URL } = process.env;
 const ENV = process.env.NODE_ENV || 'development';
 
 const baseConfig = {
-  client: 'pg',
-  migrations: {
-    directory: './db/migrations'
-  },
-  seeds: {
-    directory: './db/seeds'
-  }
+    client: 'pg',
+    migrations: {
+        directory: './db/migrations'
+    },
+    seeds: {
+        directory: './db/seeds'
+    }
 };
 
 const customConfig = {
-  development: {
-    connection: {
-      database: 'nc_news',
-      user: 'satty',
-      password: 'password'
+    development: {
+        connection: {
+            database: 'nc_news',
+            user: 'satty',
+            password: 'password'
+        }
+    },
+    test: {
+        connection: {
+            database: 'nc_news_test',
+            user: 'satty',
+            password: 'password'
+        }
+    },
+    production: {
+        connection: `${DB_URL}?ssl=true`,
     }
-  },
-  test: {
-    connection: {
-      database: 'nc_news_test',
-      user: 'satty',
-      password: 'password'
-    }
-  }
 };
 
-module.exports = { ...customConfig[ENV], ...baseConfig };
+module.exports = {...customConfig[ENV], ...baseConfig };
