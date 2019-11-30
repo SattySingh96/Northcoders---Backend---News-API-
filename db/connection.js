181e1a7989da9e82adc429de9c8dc36bb03fd948
Satnam Singh <Satnam.Bhatti.Singh96@gmail.com>
2019-11-30 11:03:45 +0000

    added require app to listern.js

--- db/connection.js
+++ db/connection.js
@@ -2,8 +2,7 @@ const ENV = process.env.NODE_ENV || 'development';
 const knex = require('knex');
 
 const dbConfig =
-    ENV === 'production' ?
-    { client: 'pg', connection: process.env.DATABASE_URL } :
+    ENV === 'production' ? { client: 'pg', connection: process.env.DATABASE_URL } :
     require('../knexfile');
 
 module.exports = connection;
